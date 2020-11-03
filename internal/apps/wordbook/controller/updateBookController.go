cb57443b591bb66bb24278410570d68736b0f66a
Shinpe1 <Shinpei8080@gmail.com>
2020-11-03 23:09:47 +0900

    コンテンツ更新処理

--- internal/apps/wordbook/controller/updateBookController.go
+++ internal/apps/wordbook/controller/updateBookController.go
@@ -1,7 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
+	. "github.com/Shinpe1/wordbook_web/internal/apps/wordbook/model/request"
+	. "github.com/Shinpe1/wordbook_web/internal/apps/wordbook/services"
+	"github.com/gin-gonic/gin"
+)
+
+/** 単語帳の更新を行います */
 func UpdateBookController(ctx *gin.Context) {
+	// リクエストボディの取得
+	var request UpdateBookComp
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"Invalid Parameters": err.Error()})
+	}
+
+	// 指定された単語帳の内容を更新します
+	UpdateBookService(&request)
 
 }
