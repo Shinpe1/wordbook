f7ee0fa0384334095cce01e5b40d947ecc4e6ea4
Shinpe1 <Shinpei8080@gmail.com>
2020-11-14 13:29:18 +0900

    アクセストークン実装

--- internal/apps/wordbook/application.go
+++ internal/apps/wordbook/application.go
@@ -1,59 +1,79 @@
 package wordbook
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/Shinpe1/wordbook_web/internal/apps/wordbook/controller"
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
+
+	. "github.com/Shinpe1/wordbook_web/auth"
 )
 
 func Run() {
 	router := gin.Default()
 
 	// コンテクストパス
-	originPath := router.Group("/api/v1.0")
+	const CONTEXT_PATH = "/api/v1.0"
+	ORIGIN_PATH := router.Group(CONTEXT_PATH)
 	{
 		// 取得API
-		listAPI := originPath.Group("/list")
+		listUrl := ORIGIN_PATH.Group("/list", AuthMiddleware.MiddlewareFunc())
 		{
 			// 単語帳一覧取得
-			listAPI.GET("/all", controller.GetBookListController)
+			listUrl.GET("/all", controller.GetBookListController)
 			// 個別単語帳取得
-			listAPI.GET("/individual", controller.GetIndividualBookController)
+			listUrl.GET("/individual", controller.GetIndividualBookController)
 		}
 		// 登録API
-		registerUrl := originPath.Group("/register")
+		registerUrl := ORIGIN_PATH.Group("/register", AuthMiddleware.MiddlewareFunc())
 		{
 			// 単語帳新規登録
 			registerUrl.POST("", controller.RegisterBookController)
 		}
 		// 更新API
-		updateUrl := originPath.Group("/update")
+		updateUrl := ORIGIN_PATH.Group("/update", AuthMiddleware.MiddlewareFunc())
 		{
 			// 単語帳更新
 			updateUrl.POST("", controller.UpdateBookController)
 		}
 		// 追加API
-		insertUrl := originPath.Group("/insert")
+		insertUrl := ORIGIN_PATH.Group("/insert", AuthMiddleware.MiddlewareFunc())
 		{
 			// 単語帳追加
 			insertUrl.POST("", controller.InsertBookController)
 		}
 		// 削除API
-		deleteUrl := originPath.Group("/delete")
+		deleteUrl := ORIGIN_PATH.Group("/delete", AuthMiddleware.MiddlewareFunc())
 		{
 			// 単語帳削除
 			deleteUrl.POST("", controller.DeleteBookController)
 		}
 		//認証API
-		authUrl := originPath.Group("/auth")
+		authUrl := ORIGIN_PATH.Group("/auth")
 		{
 			// ユーザー登録
 			authUrl.POST("/register", controller.RegisterUserController)
 			// ログイン
-			authUrl.POST("/login", controller.LoginUserController)
+			// authUrl.POST("/login", controller.LoginUserController)
+			authUrl.POST("/login", AuthMiddleware.LoginHandler)
 			// トークンリフレッシュ
-			// authAPI.POST("/refresh", controller.RefreshTokenController)
+			authUrl.POST("/refresh", AuthMiddleware.RefreshHandler)
 		}
 	}
 
-	router.Run(":8000")
+	router.NoRoute(AuthMiddleware.MiddlewareFunc(), func(ctx *gin.Context) {
+		claims := jwt.ExtractClaims(ctx)
+		log.Println("NoRoute Claims: {}", claims)
+		ctx.JSONP(http.StatusNotFound, gin.H{
+			"code":    "PAGE NOT FOUND",
+			"message": "page not found",
+		})
+	})
+
+	err := router.Run(":8000")
+	if err != nil {
+		panic(err.Error())
+	}
 }
