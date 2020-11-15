8ac9d4cf9629c43bc8936aca727e6d1f78ee28f8
p3ys1n <diggy_world@yahoo.co.jp>
2020-11-15 21:12:44 +0900

    modify db connection

--- settings/db/connection.go
+++ settings/db/connection.go
@@ -13,13 +13,11 @@ func ConnectDB() (database *gorm.DB, err error) {
 	DBMS := "mysql"
 	USER := os.Getenv("DB_USER")
 	PASS := os.Getenv("DB_PASS")
-	PROTOCOL := os.Getenv("DB_PROTOCOL")
-	// PROTOCOL := "tcp(localhost:3306)"
+	HOSTNAME := os.Getenv("DB_HOSTNAME")
 	DBNAME := os.Getenv("DB_NAME")
-	// DBNAME := "wordbook"
 
 	// datetime型をtime.Timeで受け取れるようにするため,parseTime=trueを指定している
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&reconnect=true&parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := USER + ":" + PASS + "@" + HOSTNAME + "/" + DBNAME + "?charset=utf8&reconnect=true&parseTime=true&loc=Asia%2FTokyo"
 
 	db, err := gorm.Open(DBMS, CONNECT)
 	if err != nil {
